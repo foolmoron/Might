5cef8e9dc87265a70a94c5ec82391d14951c71c7
Momin Khan <foolmoron@gmail.com>
2018-01-27 13:07:44 -0500

    Basic asset structures

---
+++ Assets/Scripts/CreatureObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CreatureAction {
+    Attack,
+    Magic,
+    Defend,
+    Talk,
+}
+
+[Serializable]
+public class CreaturePhase {
+    public CreatureAction GoodAction;
+    [TextArea]
+    public string GoodReaction = "The creature was damaged!";
+    public CreatureAction BadAction;
+    [TextArea]
+    public string BadReaction = "The creature hurt you!";
+    [TextArea]
+    public string NeutralReaction = "Nothing happened...";
+}
+[Serializable]
+public class Dialogue {
+    [TextArea]
+    public string Text = "Hi I am a creature";
+    [Range(-1, 1)]
+    public int Score = 0;
+}
+
+[CreateAssetMenu(fileName = "rawr", menuName = "MIGHT/Creature", order = 0)]
+public class CreatureObject : ScriptableObject {
+    public Sprite Sprite;
+    public List<CreaturePhase> Pattern;
+    public List<Dialogue> Dialogues;
+}
---
+++ Assets/Scripts/QuestionObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Answer {
+    [TextArea]
+    public string Text = "42";
+    [Range(-1, 1)]
+    public int Score = -1;
+}
+
+[CreateAssetMenu(fileName = "what", menuName = "MIGHT/Question", order = 1)]
+public class QuestionObject : ScriptableObject {
+    [TextArea]
+    public string Question = "What is the meaning of life?";
+    public Answer Answer1;
+    public Answer Answer2;
+    public Answer Answer3;
+    public Answer Answer4;
+}
