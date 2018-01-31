c2c7be9b4ff64079d043694972d98dd8a7e4eec5
Momin Khan <foolmoron@gmail.com>
2018-01-30 20:42:28 -0500

    audio fix

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour {
+
+    AudioSource audio;
+    public float PlayDelay;
+    public float UnmuteDelay;
+    public AudioSource MuteTarget;
+
+    void Awake() {
+        audio = GetComponent<AudioSource>();
+        audio.Stop();
+        audio.mute = true;
+    }
+
+    void FixedUpdate() {
+        if (PlayDelay > 0) {
+            PlayDelay -= Time.deltaTime;
+            if (PlayDelay <= 0) {
+                audio.Play();
+            }
+        }
+        if (UnmuteDelay > 0) {
+            UnmuteDelay -= Time.deltaTime;
+            if (UnmuteDelay <= 0) {
+                audio.mute = false;
+                MuteTarget.mute = true;
+            }
+        }
+    }
+}
